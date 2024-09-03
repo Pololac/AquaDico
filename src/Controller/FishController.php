29fe316965502f66ff6256ecb15985cbb5d9bf60
Pololac <lacostepaul@yahoo.fr>
2024-09-03 23:34:48 +0200

    Ajout form AddFish et début paramétrages f° Upload image jusqu'à vue (à refactoriser)

--- src/Controller/FishController.php
+++ src/Controller/FishController.php
@@ -3,8 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\Fish;
+use App\Form\AddFishType;
 use App\Form\SearchType;
 use App\Repository\FishRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -12,11 +14,13 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class FishController extends AbstractController
 {
+    // AFFICHAGE DE TOUTES LES FICHES OU DE CELLES QUI CORRESPONDENT A LA RECHERCHE
     #[Route('/poissons', name: 'fishes_list', methods: ['GET'])]
     public function list(Request $request, FishRepository $fishRepository): Response
     {
         $fishes = $fishRepository->findAll();
 
+        //BARRE DE RECHERCHE
         $form = $this->createForm(SearchType::class);
                 
         $form->handleRequest($request);
@@ -30,7 +34,7 @@ class FishController extends AbstractController
             $fishes = $fishRepository->findBySearchQuery($query);
 
             if (empty($fishes)) {
-                $this->addFlash('notice', 'Aucun poisson trouvé pour cette recherche.');
+                $this->addFlash('info', 'Aucun poisson trouvé pour cette recherche.');
             }
 
            return $this->render('fish/list.html.twig', [
@@ -46,8 +50,8 @@ class FishController extends AbstractController
         ]);
     }
 
-
-    #[Route('/poissons/{id}', name: 'fish_item')]
+    // AFFICHAGE DE LA FICHE D'UN POISSON
+    #[Route('/poisson/{id}', name: 'fish_item')]
     public function item(Fish $fish): Response
     {        
          return $this->render('fish/item.html.twig', [
@@ -55,6 +59,40 @@ class FishController extends AbstractController
         ]);     // ERREUR 404 générée automatiquement
 
     }
+
+    //AJOUT D'UNE FICHE DE POISSON
+    #[Route('/poissons/add', name: 'fishes_add', methods: ['GET', 'POST'])]
+    public function addFish(
+        Request $request,
+        EntityManagerInterface $em,
+        ): Response
+    {   
+        $fish = new Fish();
+        $form = $this->createForm(AddFishType::class, $fish);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->persist($fish);
+            $em->flush();
+
+            $this->addFlash('success', 'Votre fiche a bien été ajoutée. Merci!');
+            
+            return $this->render('fish/item.html.twig', [
+                'fish' => $fish,
+        ]);     // ERREUR 404 générée automatiquement
+
+            // return $this->redirectToRoute('add_confirm');
+        }    
+
+         return $this->render('fish/add.html.twig', [
+                'addFish' => $form,
+        ]);     // ERREUR 404 générée automatiquement
+
+    }
+
+
+
 
 
     // #[Route('/poissons/{name}', name: 'fishfamily_item')]
---
+++ src/Form/AddFishType.php
@@ -0,0 +1,212 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Fish;
+use App\Entity\FishFamily;
+use App\Entity\Origin;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Callback;
+use Symfony\Component\Validator\Constraints\Image;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Range;
+use Symfony\Component\Validator\Context\ExecutionContextInterface;
+
+class AddFishType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name', TextType::class, [
+                'label' => 'Nom',
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Le nom ne peut pas être vide.',
+                    ]),
+                    new Length([
+                        'max' => 50,
+                        'maxMessage' => 'Le nom ne doit pas dépasser {{ limit }} caractères.',
+                    ]),
+                ],
+            ])
+            ->add('latinName', TextType::class, [
+                'label' => 'Nom Latin',
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Le nom latin ne peut pas être vide.',
+                    ]),
+                    new Length([
+                        'max' => 50,
+                        'maxMessage' => 'Le nom latin ne doit pas dépasser {{ limit }} caractères.',
+                    ]),
+                ],
+            ])
+            ->add('description', TextareaType::class, [
+                'label' => 'Description',
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'La description ne peut pas être vide.',
+                    ]),
+                    new Length([
+                        'max' => 1000,
+                        'maxMessage' => 'La description ne doit pas dépasser {{ limit }} caractères.',
+                    ]),
+                ],
+            ])
+            ->add('adultSize', NumberType::class, [
+                'label' => 'Taille Adulte (en cm)',
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'La taille adulte est requise.',
+                    ]),
+                    new Range([
+                        'min' => 1,
+                        'max' => 100,
+                        'notInRangeMessage' => 'La taille adulte doit être comprise entre 1 et 100 cm.',
+
+                    ]),
+                ],
+            ])
+            ->add('minTemp', NumberType::class, [
+                'label' => 'Température Minimum (°C)',
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'La température minimum est requise.',
+                    ]),
+                ],
+            ])
+            ->add('maxTemp', NumberType::class, [
+                'label' => 'Température Maximum (°C)',
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'La température maximum est requise.',
+                    ]),
+                    new Callback([$this, 'validateTemperature']),
+                ],
+            ])
+            ->add('minPh', NumberType::class, [
+                'label' => 'pH Minimum',
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Le pH minimum est requis.',
+                    ]),
+                    new Range([
+                        'min' => 1,
+                        'max' => 14,
+                        'notInRangeMessage' => 'Le pH doit être compris entre 1 et 14.',
+                    ]),
+                ],
+            ])
+            ->add('maxPh', NumberType::class, [
+                'label' => 'pH Maximum',
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Le pH maximum est requis.',
+                    ]),
+                    new Range([
+                        'min' => 1,
+                        'max' => 14,
+                        'notInRangeMessage' => 'Le pH doit être compris entre 1 et 14.',
+                    ]),
+                    new Callback([$this, 'validatePh']),
+                ],
+            ])
+            ->add('minGh', NumberType::class, [
+                'label' => 'GH Minimum',
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Le GH minimum est requis.',
+                    ]),
+                    new Range([
+                        'min' => 1,
+                        'max' => 34,
+                        'notInRangeMessage' => 'Le Gh doit être compris entre 1 et 34.',
+                    ]),
+                ],
+            ])
+            ->add('maxGh', NumberType::class, [
+                'label' => 'GH Maximum',
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Le GH maximum est requis.',
+                    ]),
+                    new Range([
+                        'min' => 1,
+                        'max' => 34,
+                        'notInRangeMessage' => 'Le Gh doit être compris entre 1 et 34.',
+                    ]),
+                    new Callback([$this, 'validateGh']),
+                ],
+            ])
+
+            ->add('picFilename', FileType::class, [
+                'label' => 'Image',
+                'mapped' => false,
+                'required' => false,
+                'constraints' => [
+                    new Image()
+                ],
+            ])
+            ->add('family', EntityType::class, [
+                'class' => FishFamily::class,
+                'choice_label' => 'name',
+            ])
+            ->add('origin', EntityType::class, [
+                'class' => Origin::class,
+                'choice_label' => 'continent',
+            ])
+            
+            ->add('submit', SubmitType::class, [
+                'label'=> 'Ajout'
+            ]);
+    }
+
+    public function validateTemperature($value, ExecutionContextInterface $context)
+    {
+        $form = $context->getRoot();
+        $minTemp = $form->get('minTemp')->getData();
+        if ($value <= $minTemp) {
+            $context->buildViolation('La température maximale doit être supérieure à la température minimale.')
+                ->addViolation();
+        }
+    }
+
+    public function validatePh($value, ExecutionContextInterface $context)
+    {
+        $form = $context->getRoot();
+        $minPh = $form->get('minPh')->getData();
+        if ($value <= $minPh) {
+            $context->buildViolation('Le pH maximal doit être supérieur au pH minimal.')
+                ->addViolation();
+        }
+    }
+
+    public function validateGh($value, ExecutionContextInterface $context)
+    {
+        $form = $context->getRoot();
+        $minGh = $form->get('minGh')->getData();
+        if ($value <= $minGh) {
+            $context->buildViolation('Le GH maximal doit être supérieur au GH minimal.')
+                ->addViolation();
+        }
+    }
+
+
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Fish::class,
+        ]);
+    }
+}
