2aa0f36a59ffae981d95908bd8008375654bf646
Pololac <lacostepaul@yahoo.fr>
2025-05-30 00:03:51 +0200

    Small fixes

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,8 @@ const path = require('path');
 module.exports = {
     content: [
       "./templates/**/*.html.twig",
-      "./vendor/tales-from-a-dev/flowbite-bundle/templates/**/*.html.twig",
       "./assets/**/*.js",
+      "./vendor/tales-from-a-dev/flowbite-bundle/templates/**/*.html.twig",
     ],
     darkMode: 'class',
     theme: {
